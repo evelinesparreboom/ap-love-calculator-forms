1031e2f4b11df5687e0b37a05d30478c6cd1b402
Wouter Crooy <wouter.crooy@albumprinter.com>
2017-04-06 23:55:28 +0200

    Added blink funct via async

--- forms_ap_love_calculator/MainPage.xaml.cs
+++ forms_ap_love_calculator/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using Lamp.Plugin;
 using Xamarin.Forms;
 
 namespace forms_ap_love_calculator
@@ -12,18 +13,41 @@ namespace forms_ap_love_calculator
 			InitializeComponent();
 		}
 
-		void OnCalculate(object sender, EventArgs e)
+		async void OnCalculate(object sender, EventArgs e)
 		{
 			var loveResult = Core.ApLove.ToNumber(person1nameText.Text, person2nameText.Text);
 			if (loveResult == "infinity")
 			{
 				loveResultLabel.Text = "Your love is incalculable";
 			}
-			else
-			{
-				loveResultLabel.Text = loveResult + "%";
-			}
-		}
+	        else
+	        {
+	            loveResultLabel.Text = loveResult + "%";
+	            int loveResultPercentage = 0;
+	            if (int.TryParse(loveResult, out loveResultPercentage) && loveResultPercentage > 50)
+	            {
+	                await Blink();
+	            }
+	        }
+	    }
 
-	}
+
+	    async Task Blink()
+	    {
+	        for (int i = 0; i < 25; i++)
+	        {
+	            if (i % 2 == 0)
+	            {
+	                CrossLamp.Current.TurnOn();
+	            }
+	            else
+	            {
+	                CrossLamp.Current.TurnOff();
+	            }
+	            await Task.Delay(700);
+	        }
+            CrossLamp.Current.TurnOff();
+	    }
+
+    }
 }
